904cbc55373dc3094e13f031b055998c5f25807f
X <phi@gmx.io>
2025-09-17 15:55:48 +0800

    update home page

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ import { RandomQuote } from 'components/random-quote';
 import { getNetlifyContext } from 'utils';
 
 const contextExplainer = `
-The card below is rendered on the server based on the value of \`process.env.CONTEXT\` 
+The card below is rendered on the server based on the value of \`process.env.CONTEXT\`
 ([docs](https://docs.netlify.com/configure-builds/environment-variables/#build-metadata)):
 `;
 
@@ -28,11 +28,7 @@ export default function Page() {
         <div className="flex flex-col gap-12 sm:gap-16">
             <section>
                 <ContextAlert className="mb-6" />
-                <h1 className="mb-4">Netlify Platform Starter - Next.js</h1>
-                <p className="mb-6 text-lg">Get started with Next.js and Netlify in seconds.</p>
-                <Link href="https://docs.netlify.com/frameworks/next-js/overview/" className="btn btn-lg sm:min-w-64">
-                    Read the Docs
-                </Link>
+                <h1 className="mb-4">Home</h1>
             </section>
             {!!ctx && (
                 <section className="flex flex-col gap-4">
